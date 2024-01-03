972e0a8db146bca7e0b1f57a6e3dfd7ddca0bb2c
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-01-02 22:14:29 -0600

    separamos el javascript del html, y lo pegamos en la hoja .js

--- javascript/archivojava.js
+++ javascript/archivojava.js
@@ -1,3 +1,63 @@
-a=1
-b=2
-alert("El valor de c es " (a+b));
+
+
+function aleatorio (min, max){
+    return Math.floor(Math.random()*(max-min)+min)
+}
+
+function eleccion (jugada){
+    let resultado = ""
+
+        if(jugada == 1){
+            resultado= "Piedra 🗿"
+        } else if(jugada == 2){
+            resultado= "Papel 🧻"
+        } else if(jugada == 3){
+            resultado= "Tijera ✂️"
+        }else{
+            resultado = "MAL ELEGIDO"
+        }
+
+        return resultado
+}
+
+        //1 es piedra 2 es papel 3 es tijera
+        let jugador = 0
+
+        let triunfos = 0
+        let perdidas = 0
+
+
+
+        while (triunfos <3 &&perdidas <3){
+
+
+        let pc = aleatorio (1,3)
+        jugador = prompt ("elige 1 piedra, 2 papel, 3 tijera")
+
+
+
+       alert("PC elige: " + eleccion(pc))
+       alert ("Tu eliges: " + eleccion(jugador))
+       //COMBATE
+       //encontrar todas las maneras en que se pueden ganar
+
+       if(pc==jugador){
+        alert("Empate")
+       } else if (jugador==1 && pc == 3){
+        alert("Ganaste")
+        triunfos = triunfos +1
+       } else if (jugador == 2 && pc == 1){
+        alert("Ganaste")
+        triunfos = triunfos +1
+       } else if (jugador == 3 && pc == 2){
+        alert("Ganaste")
+        triunfos = triunfos +1
+       }else {
+        alert ("Perdiste")
+        perdidas=perdidas+1
+       }
+
+    }
+
+    alert("Ganaste " + triunfos + " veces. Perdiste " + perdidas + " veces.")
+            
